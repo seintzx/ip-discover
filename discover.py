8c49e235eb9a9b5a1de9039e10deec7b7c5e977d
seintzx <seintzx@gmail.com>
2019-09-12 17:22:13 +0200

    add machinae option

--- discover.py
+++ discover.py
@@ -6,22 +6,30 @@ from my_dig import my_dig
 from my_machinae import my_machinae
 
 
-def init_args():
+def main():
     parser = argparse.ArgumentParser(
         description="IP/Domain information discovery tool")
     parser.add_argument("IP", help="IP address to lookup")
+    parser.add_argument("-m",
+                        "--machinae",
+                        action="store_true",
+                        help="Also use machinae")
     args = parser.parse_args()
-    return (args.IP)
+    IP = args.IP
 
+    if args.machinae:
+        print("- machinae ============================")
+        my_machinae(IP)
 
-def main():
-    IP = init_args()
-    print("whois ===============================")
+    print("\n- **whois**")
+    print("\t```".expandtabs(4))
     my_whois(IP)
-    print("dig =================================")
+    print("\t```".expandtabs(4))
+
+    print("- **dig**")
+    print("\t```".expandtabs(4))
     my_dig(IP)
-    print("machinae ============================")
-    my_machinae(IP)
+    print("\t```".expandtabs(4))
 
 
 if __name__ == "__main__":
