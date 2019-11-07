dddbe2abc6091b53c300b3f80ffd740b77337f5c
seintzx <seintzx@gmail.com>
2019-11-07 17:19:52 +0100

    add arg parse module

---
+++ my_arg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+def my_arg():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="IP/Domain information discovery tool")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-a",
+                       "--hash",
+                       action="store",
+                       help="search hash on virustotal")
+    group.add_argument("-i",
+                       "--ip",
+                       action="store",
+                       help="IP address or domain to lookup")
+    parser.add_argument("-m",
+                        "--machinae",
+                        action="store_true",
+                        help="also use machinae")
+
+    return (parser.parse_args())
